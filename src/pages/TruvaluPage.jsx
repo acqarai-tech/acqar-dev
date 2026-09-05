9dd0cdc11ae0e8a96a930391b7ac40f0c49c10ea
acqarai-tech <acqarai@gmail.com>
2026-09-05 08:18:07 +0500

    Update TruvaluPage.jsx

--- src/pages/TruvaluPage.jsx
+++ src/pages/TruvaluPage.jsx
@@ -2,7 +2,7 @@ import { useState,useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { trackEvent } from "../analytics";
 // import { Helmet } from "react-helmet-async";
-import Nav from '../components/NavBar';
+import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 import "../styles/truvalu-dark.css";
 
